62a4fb9c217cd3a6d1e4cf200b9ee4f7372a7533
akash23-ai <akashbhardwaj2024@gmail.com>
2024-01-16 15:15:26 +0530

    Made a live video app

---
+++ liveVideo/index.js
@@ -0,0 +1,116 @@
+async function main() {
+  const myVideo = document.getElementById("myVideo");
+  const buttonStart = document.getElementById("start");
+  const stopBtn = document.getElementById("offBtn");
+  const startLiveBtn = document.getElementById("startLive");
+  const stopLiveBtn = document.getElementById("offBtnLive");
+  const recordedVideo = document.getElementById("recordedVideo");
+
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: {
+        exact: "user",
+      },
+    },
+    audio: true,
+  });
+
+  // First create stream object
+
+  startLiveBtn.addEventListener("click", startLive);
+
+  async function startLive() {
+    if (!stream.active) {
+      console.log("inside");
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          facingMode: {
+            exact: "user",
+          },
+        },
+        audio: true,
+      });
+    }
+    console.log(stream);
+    myVideo.srcObject = stream;
+
+    buttonStart.removeAttribute("disabled");
+    stopLiveBtn.removeAttribute("disabled");
+  }
+
+  stopLiveBtn.addEventListener("click", stopLive);
+
+  function stopLive() {
+    const stream = myVideo.srcObject;
+    var tracks = stream.getTracks();
+
+    console.log(tracks);
+
+    tracks.forEach((track) => {
+      track.stop();
+    });
+
+    myVideo.srcObject = null;
+
+    stopBtn.setAttribute("disabled", true);
+    recordedVideo.setAttribute("controls", true);
+
+    stopLiveBtn.setAttribute("disabled", true);
+  }
+
+  // myVideo.onloadedmetadata = (e) => {
+  //   myVideo.play();
+  // };
+
+  // record the live video
+
+  if (!MediaRecorder.isTypeSupported("video/webm")) {
+    console.log("Not supported");
+
+    alert("Not supported");
+  }
+
+  // let mediaRecoder = new MediaRecorder(stream, {
+  //   mimeType: "video/webm",
+  // });
+
+  // This is it
+
+  buttonStart.addEventListener("click", startBtnFn);
+
+  function startBtnFn() {
+    let mediaRecoder = new MediaRecorder(stream, {
+      mimeType: "video/webm",
+    });
+
+    if (mediaRecoder.mimeType !== "video/webm") {
+      console.log("inside media recoder");
+      mediaRecoder = new MediaRecorder(stream, {
+        mimeType: "video/webm",
+      });
+    }
+
+    mediaRecoder.start();
+
+    mediaRecoder.addEventListener("dataavailable", (event) => {
+      console.log("whats up guys i am here");
+      recordedVideo.src = URL.createObjectURL(event.data);
+    });
+
+    stopBtn.removeAttribute("disabled");
+    buttonStart.setAttribute("disabled", true);
+  }
+
+  stopBtn.addEventListener("click", stopBtnFn);
+
+  function stopBtnFn() {
+    mediaRecoder.stop();
+    buttonStart.setAttribute("disabled", true);
+    stopBtn.setAttribute("disabled", true);
+    if (!recordedVideo.hasAttribute("controls")) {
+      recordedVideo.setAttribute("controls", true);
+    }
+  }
+}
+
+main();
